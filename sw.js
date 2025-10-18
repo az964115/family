a21832e5ccdfc945b50ef16bce968e4a3ddd91d4
az964115 <98249687+az964115@users.noreply.github.com>
2025-10-18 10:26:50 +0800

    Remove service worker event listeners

--- sw.js
+++ sw.js
@@ -1,19 +1,3 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("piggy-cache-v2")  // 👈 改這個版本號
-      .then(cache => cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "https://cdn.jsdelivr.net/npm/chart.js"
-      ]))
-  );
-});
 
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
 
