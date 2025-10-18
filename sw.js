fa0c4914d8c18d2f6edc7a3cbd3757687ae5ad35
az964115 <98249687+az964115@users.noreply.github.com>
2025-10-18 09:57:58 +0800

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("piggy-cache-v1").then(cache => {
-      return cache.addAll([
+    caches.open("piggy-cache-v2")  // 👈 改這個版本號
+      .then(cache => cache.addAll([
         "./",
         "./index.html",
         "./manifest.json",
         "https://cdn.jsdelivr.net/npm/chart.js"
-      ]);
-    })
+      ]))
   );
 });
+
 
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
+
